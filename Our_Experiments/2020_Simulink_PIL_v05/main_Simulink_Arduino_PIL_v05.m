68b3ceecf40a56dff5bcec6bd03362ef77637acd
liamw99 <liamwarburton99@hotmail.com>
2020-04-30 15:34:53 +1000

    changed to 3 outputs

--- Our_Experiments/2020_Simulink_PIL_v05/main_Simulink_Arduino_PIL_v05.m
+++ Our_Experiments/2020_Simulink_PIL_v05/main_Simulink_Arduino_PIL_v05.m
@@ -4,9 +4,9 @@ simu="Simulink_Arduino_PIL_v05";     % Simulink file name
 
 %% Simulation Settings
 simulate= true; % True: To simulate
-Tsim = 120;        % Total Simulation length in seconds. 
+Tsim = 10;        % Total Simulation length in seconds. 
                  % Set Tsim=Inf to run indefinitely                            
-fs=25;          % Sampling Frequency in Hz
+fs=50;          % Sampling Frequency in Hz
 
 Ts=1/fs;         % Sampling Period
 
@@ -20,7 +20,7 @@ PIL=0;          %0: Manually start the PIL controller
                 %   from the beginning of the simulation
 
 %% Model Constant Parameters
-I = 8*exp(-4);
+I = 0.146;
 I1 = I;
 I2 = I;
 m = 0.35;
@@ -37,15 +37,15 @@ G = 300; %amplifier/motor voltage to torque gain.
 y = 0.4;                
                 
 %% Initial Condition
-w1o = 0;
-h1o = w1o*I;
-w2o = 0;
-h2o = w1o*I;
-xco = 0;
-xeo = 0;
-x1o = 0;
-po = 0;
-xo = [h1o h2o xco xeo x1o po]'; %State initial condition
+
+O1_dot_o = 0;
+O1_o = 0;
+O2_dot_o = 0;
+O2_o = 0;
+x_dot_o = 0;
+x_o = 0;
+
+xo = [O1_dot_o, O1_o, O2_dot_o, O2_o, x_dot_o, x_o]'; %State initial condition
 xo_hat=[0 0 0 0 0 0]';          %Observer initial condition
 
 
@@ -65,7 +65,8 @@ Ac = [-b1*I1 -(3/2)*k*r^2*I1 0 (3/2)*k*r^2*I1 0 (sqrt(3)/2)*k*r*I1; %O1_ddot
 Bc = [I1 0 0 0 0 0;
     0 I2  0 0 0 0]'
   
-Cc = [0.5 0 0.5 0 0 0;
+Cc = [1 0 0 0 0 0;
+      0 0 1 0 0 0;
       0 0 0 0 0 1];
 
 CO = ctrb(Ac,Bc);
@@ -87,17 +88,17 @@ B = sys_dt.B;
 C = sys_dt.C;
 
 %% Poles
-os = 9;
-tsettle = 5.5;
+os = 7;
+tsettle = 4.5;
 zeta = -log(os/100)\(sqrt(pi^2+log(os/100)^2))
 wn=-log(0.02*sqrt(1-zeta^2))/(zeta*tsettle)
 
 %Dominant second order poles
 s_poles = [-zeta*wn+wn*sqrt(zeta^2-1),-zeta*wn-wn*sqrt(zeta^2-1)]
 
-Pc = [s_poles(1) conj(s_poles(1)) 4*real(s_poles(1)) 4.2*real(s_poles(1)) 4.4*real(s_poles(1)) 4.6*real(s_poles(1))]; 
-%Pc = [-0.707+0.707*i, -0.707-0.707*i, -4-4i,-4+4i, -4.2-4.2i, -4.2+4.2i]/8.5; 
-%Pc = [-0.8 -1 -1.2 -1.4 -1.6 -1.8];
+%Pc = [s_poles(1) conj(s_poles(1)) 4*real(s_poles(1)) 4.2*real(s_poles(1)) 4.4*real(s_poles(1)) 4.6*real(s_poles(1))]; 
+Pc = [-0.707+0.707*i, -0.707-0.707*i, -4-4i,-4+4i, -4.2-4.2i, -4.2+4.2i]; 
+%Pc = [-0.8 -1 -1.2 -1.4 -1.6 -1.8]/10;
 Pz = exp(Pc*Ts);
 F=place(A, B, Pz);
 
@@ -118,7 +119,7 @@ if (rank_OM==n)
 
     L = place(A', C', POz)';
 else
-    disp('System is NOT Observalbe')
+    disp('System is NOT Observable')
     L = zeros(2,6);
 end
 %% Start Simulation
--- Our_Experiments/2020_Simulink_PIL_v05/plot_results.m
+++ Our_Experiments/2020_Simulink_PIL_v05/plot_results.m
@@ -2,6 +2,7 @@ clear;
 load('result_data.mat')
 y1 = y(:,1);
 y2 = y(:,2);
+y3 = y(:,3);
 u1 = u(:,1);
 u2 = u(:,2);
 
@@ -10,13 +11,13 @@ w2 = x(:,3);
 
 figure(10)
 subplot(411)
-plot(time,y1,'b', 'LineWidth',2)
+plot(time,y1,'b', time, y2, 'r', time, (y1 + y2)/2, 'g', 'LineWidth',2)
 grid
 title('Speed')
-legend('speed');
+legend('w1', 'w2', 'w3');
 
 subplot(412)
-plot(time,y2,'r', 'LineWidth',2)
+plot(time,y3,'r', 'LineWidth',2)
 grid
 title('Tension')
 legend('tension');
